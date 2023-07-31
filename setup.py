ef719469e06ccd8ec7859b20c6ee01072faf54d0
Adam Bako <bakoad@gmail.com>
2023-07-31 14:37:53 +0200

    Revert pk fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="keboola.json-to-csv",
-    version="0.0.7",
+    version="0.0.8",
     author="Keboola KDS Team",
     setup_requires=['flake8'],
     tests_require=[],
